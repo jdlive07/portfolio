ef2cf0649b44a90bc659c97588d3fd8863cb3e1b
Dawson Reid <dreid93@gmail.com>
2014-09-11 18:14:40 -0300

    Added initial project structure.

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioApp
+ * @description
+ * # portfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfolioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
